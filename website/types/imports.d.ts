e04ab064ae4b9bb72ba3a160409002101dd93a98
Malindentado <antonio@casheiro.com.br>
2025-09-10 20:40:53 -0300

    refactor: remove website directory and related files
    Clean up project by removing the entire website directory including configuration files, components, assets, and i18n resources. This includes:
    - Nuxt.js configuration files
    - Vue components and pages
    - TypeScript definitions
    - Internationalization files
    - Logo assets and CSS styles
    - Store and composable utilities

--- website/types/imports.d.ts
+++
@@ -1,7 +0,0 @@
-
-declare global {
-    const useI18n: typeof import('@nuxtjs/i18n')['useI18n']
-    const useMatrixConfig: typeof import('~/composables/useMatrixConfig')['useMatrixConfig']
-}
-
-export {}
